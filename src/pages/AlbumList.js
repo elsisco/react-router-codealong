7f951902d9c2bcf5e5fc5112db14a4bcbbcea5bf
elsisco <elsa.carlstrom@gmail.com>
2021-11-13 21:25:08 +0100

    codealong done

---
+++ src/pages/AlbumList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+export const AlbumList = () => {
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        fetch('https://theaudiodb.com/api/v1/json/1/album.php?i=112024')
+            .then((res) => res.json())
+            .then ((json) => {
+                setAlbums(json.album)
+            })
+    }, [])
+
+    return (
+        <div>
+            {albums.map((album) => (
+                <div key={album.idAlbum}>
+                    <img src={`${album.strAlbumThumb}/preview`} alt={album.strAlbum} />
+                    <h2>{album.strAlbum}</h2>
+                    <h3>
+                        <Link to={`/albums/${album.idAlbum}`}>{album.strAlbum}</Link>
+                    </h3>
+                </div>
+            ))}
+        </div>
+    )
+}
